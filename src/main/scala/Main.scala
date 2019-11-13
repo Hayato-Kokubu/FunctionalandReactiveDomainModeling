56947ce2ec1f923652ac05de7ddf29a41c9ddbad
hayatokokubu <kokubu.hayato@gmail.com>
2019-11-13 14:54:55 +0900

    wip

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,3 +3,10 @@ object Main extends App {
 
 
 }
+
+
+for {
+  a <- Seq(1,2,3)
+  b <- Seq("a","b","c")
+}yield a + b
+
--- src/main/scala/service/AccountService.scala
+++ src/main/scala/service/AccountService.scala
@@ -1,11 +1,20 @@
 package service
 
+import java.time.LocalDate
 import domain.model.{Account, Amount}
 
-trait AccountService {
-  def transfer(
-    from: Account,
-    to: Account,
-    amount: Amount
-  ): Option[Amount]
+import scala.util.Try
+
+trait AccountService[Account, Amount, Balance] {
+  def open(no: String, name: String, openDate: Option[LocalDate]): Try[Account]
+  def close(account: Account, closeDate: Option[LocalDate]): Try[Account]
+  def debit(account: Account, amount: Amount): Try[Account]
+  def credit(account: Account, amount: Amount): Try[Account]
+  def balance(account: Account): Try[Balance]
+
+  def transfer(from: Account, to: Account, amount: Amount):
+  Try[(Account, Account, Amount)] = for {
+    a <- debit(from, amount)
+    b <- credit(to, amount)
+  } yield (a, b, amount)
 }
